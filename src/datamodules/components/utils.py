182c97ed1e77d1f413fc82845c84e37c89705eeb
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2022-08-27 14:48:34 +0700

    add model and dataset module

---
+++ src/datamodules/components/utils.py
@@ -0,0 +1,166 @@
+"""
+Dataset utils
+"""
+
+from pathlib import Path
+import PIL
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import patches
+
+from torch.utils.data import Dataset
+import torch
+
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+
+def get_rectangle_edges_from_pascal_bbox(bbox):
+    xmin_top_left, ymin_top_left, xmax_bottom_right, ymax_bottom_right = bbox
+
+    bottom_left = (xmin_top_left, ymax_bottom_right)
+    width = xmax_bottom_right - xmin_top_left
+    height = ymin_top_left - ymax_bottom_right
+
+    return bottom_left, width, height
+
+
+def draw_pascal_voc_bboxes(
+    plot_ax,
+    bboxes,
+    get_rectangle_corners_fn=get_rectangle_edges_from_pascal_bbox,
+):
+    for bbox in bboxes:
+        bottom_left, width, height = get_rectangle_corners_fn(bbox)
+
+        rect_1 = patches.Rectangle(
+            bottom_left,
+            width,
+            height,
+            linewidth=4,
+            edgecolor="black",
+            fill=False,
+        )
+        rect_2 = patches.Rectangle(
+            bottom_left,
+            width,
+            height,
+            linewidth=2,
+            edgecolor="white",
+            fill=False,
+        )
+
+        # Add the patch to the Axes
+        plot_ax.add_patch(rect_1)
+        plot_ax.add_patch(rect_2)
+
+
+def show_image(
+    image, bboxes=None, draw_bboxes_fn=draw_pascal_voc_bboxes, figsize=(10, 10)
+):
+    fig, ax = plt.subplots(1, figsize=figsize)
+    ax.imshow(image)
+
+    if bboxes is not None:
+        draw_bboxes_fn(ax, bboxes)
+
+    plt.show()
+
+
+class CarsDatasetAdaptor:
+    def __init__(self, images_dir_path, annotations_dataframe):
+        self.images_dir_path = Path(images_dir_path)
+        self.annotations_df = annotations_dataframe
+        self.images = self.annotations_df.image.unique().tolist()
+
+    def __len__(self) -> int:
+        return len(self.images)
+
+    def get_image_and_labels_by_idx(self, index):
+        image_name = self.images[index]
+        image = PIL.Image.open(self.images_dir_path / image_name)
+        pascal_bboxes = self.annotations_df[self.annotations_df.image == image_name][
+            ["xmin", "ymin", "xmax", "ymax"]
+        ].values
+        class_labels = np.ones(len(pascal_bboxes))
+
+        return image, pascal_bboxes, class_labels, index
+
+    def show_image(self, index):
+        image, bboxes, class_labels, image_id = self.get_image_and_labels_by_idx(index)
+        print(f"image_id: {image_id}")
+        show_image(image, bboxes.tolist())
+        print(class_labels)
+
+
+def get_train_transforms(target_img_size=512):
+    return A.Compose(
+        [
+            A.HorizontalFlip(p=0.5),
+            A.Resize(height=target_img_size, width=target_img_size, p=1),
+            A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ToTensorV2(p=1),
+        ],
+        p=1.0,
+        bbox_params=A.BboxParams(
+            format="pascal_voc", min_area=0, min_visibility=0, label_fields=["labels"]
+        ),
+    )
+
+
+def get_valid_transforms(target_img_size=512):
+    return A.Compose(
+        [
+            A.Resize(height=target_img_size, width=target_img_size, p=1),
+            A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ToTensorV2(p=1),
+        ],
+        p=1.0,
+        bbox_params=A.BboxParams(
+            format="pascal_voc", min_area=0, min_visibility=0, label_fields=["labels"]
+        ),
+    )
+
+
+class EfficientDetDataset(Dataset):
+    def __init__(self, dataset_adaptor, transforms=get_valid_transforms()):
+        self.ds = dataset_adaptor
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        (
+            image,
+            pascal_bboxes,
+            class_labels,
+            image_id,
+        ) = self.ds.get_image_and_labels_by_idx(index)
+
+        sample = {
+            "image": np.array(image, dtype=np.float32),
+            "bboxes": pascal_bboxes,
+            "labels": class_labels,
+        }
+
+        sample = self.transforms(**sample)
+        sample["bboxes"] = np.array(sample["bboxes"])
+        image = sample["image"]
+        pascal_bboxes = sample["bboxes"]
+        labels = sample["labels"]
+
+        _, new_h, new_w = image.shape
+        sample["bboxes"][:, [0, 1, 2, 3]] = sample["bboxes"][
+            :, [1, 0, 3, 2]
+        ]  # convert to yxyx
+
+        target = {
+            "bboxes": torch.as_tensor(sample["bboxes"], dtype=torch.float32),
+            "labels": torch.as_tensor(labels),
+            "image_id": torch.tensor([image_id]),
+            "img_size": (new_h, new_w),
+            "img_scale": torch.tensor([1.0]),
+        }
+
+        return image, target, image_id
+
+    def __len__(self):
+        return len(self.ds)
